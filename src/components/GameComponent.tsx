bb405735c6c35154ff8a01044eb337f1cfdfb202
Michael Gawrychowski <michaelgawrychowski@mac.lan>
2025-12-30 20:39:12 -0500

    Fix Phaser import syntax for ES modules

--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Phaser from "phaser";
+import * as Phaser from "phaser";
 import { createGameConfig } from "@/game/config/gameConfig";
 
 interface GameComponentProps {
